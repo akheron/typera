01a39967f9e23e7bdad62afb0b97720d687b7aa9
Petri Lehtinen <petri@digip.org>
2019-10-03 15:12:42 +0300

    Type responses in route tests

--- packages/typera-koa/tests/route.spec.ts
+++ packages/typera-koa/tests/route.spec.ts
@@ -1,5 +1,5 @@
 import * as t from 'io-ts'
-import { Parser, Response, URL, router, route } from '..'
+import { Parser, Response, Route, URL, router, route } from '..'
 import * as request from 'supertest'
 import { makeServer } from './utils'
 
@@ -14,12 +14,10 @@ describe('route', () => {
   })
 
   it('works', async () => {
-    const routes = router(
-      route('get', '/foo')()(_ => {
-        return Response.ok('foo')
-      })
-    )
-    server = makeServer(routes.handler())
+    const foo: Route<Response.Ok<string>> = route('get', '/foo')()(_ => {
+      return Response.ok('foo')
+    })
+    server = makeServer(router(foo).handler())
 
     request(server)
       .get('/foo')
@@ -27,22 +25,29 @@ describe('route', () => {
   })
 
   it('supports adding multiple routes', async () => {
-    const routes = router(
-      route('get', '/foo')()(_ => {
-        return Response.ok('foo')
-      }),
-      route('get', '/bar')()(_ => {
-        return Response.ok('bar')
-      })
-    ).add(
-      route('get', '/baz')()(_ => {
-        return Response.ok('baz')
-      }),
-      route('get', '/quux')()(_ => {
-        return Response.ok('quux')
-      })
-    )
-    server = makeServer(routes.handler())
+    const foo: Route<Response.Ok<string>> = route('get', '/foo')()(_ => {
+      return Response.ok('foo')
+    })
+    const bar: Route<Response.Ok<string>> = route('get', '/bar')()<
+      Response.Ok<string>
+    >(_ => {
+      return Response.ok('bar')
+    })
+    const baz: Route<Response.Ok<string>> = route('get', '/baz')()<
+      Response.Ok<string>
+    >(_ => {
+      return Response.ok('baz')
+    })
+    const quux: Route<Response.Ok<string>> = route('get', '/quux')()<
+      Response.Ok<string>
+    >(_ => {
+      return Response.ok('quux')
+    })
+
+    const handler = router(foo, bar)
+      .add(baz, quux)
+      .handler()
+    server = makeServer(handler)
 
     request(server)
       .get('/foo')
@@ -59,18 +64,20 @@ describe('route', () => {
   })
 
   it('decodes the request', async () => {
-    const routes = router(
-      route('post', '/decode/', URL.str('foo'))(
-        Parser.query(t.type({ bar: t.string })),
-        Parser.body(t.type({ baz: t.number }))
-      )(request => {
-        expect(request.routeParams).toEqual({ foo: 'FOO' })
-        expect(request.query).toEqual({ bar: 'BAR' })
-        expect(request.body).toEqual({ baz: 42 })
-        return Response.noContent()
-      })
-    )
-    server = makeServer(routes.handler())
+    const decode: Route<
+      Response.NoContent | Response.BadRequest<string>
+    > = route('post', '/decode/', URL.str('foo'))(
+      Parser.query(t.type({ bar: t.string })),
+      Parser.body(t.type({ baz: t.number }))
+    )(request => {
+      expect(request.routeParams).toEqual({ foo: 'FOO' })
+      expect(request.query).toEqual({ bar: 'BAR' })
+      expect(request.body).toEqual({ baz: 42 })
+      return Response.noContent()
+    })
+
+    const handler = router(decode).handler()
+    server = makeServer(handler)
 
     await request(server)
       .post('/decode/FOO?bar=BAR')
@@ -79,14 +86,15 @@ describe('route', () => {
   })
 
   it('returns errors from middleware', async () => {
-    const routes = router(
-      route('post', '/error')(Parser.body(t.type({ foo: t.number })))(
-        _request => {
-          return Response.noContent()
-        }
-      )
+    const error: Route<
+      Response.NoContent | Response.BadRequest<string>
+    > = route('post', '/error')(Parser.body(t.type({ foo: t.number })))(
+      _request => {
+        return Response.noContent()
+      }
     )
-    server = makeServer(routes.handler())
+    const handler = router(error).handler()
+    server = makeServer(handler)
 
     await request(server)
       .post('/error')
