691c465c0e0e6106d67fd73f567d3928a34db6ee
Petri Lehtinen <petri@digip.org>
2021-02-16 21:21:30 +0200

    wrapNative: Add tests for helmet

--- packages/typera-express/tests/wrapNative.spec.ts
+++ packages/typera-express/tests/wrapNative.spec.ts
@@ -1,10 +1,11 @@
 import * as cors from 'cors'
+import * as helmet from 'helmet'
 import { Middleware, Response, router, route } from '..'
 import * as request from 'supertest'
 import { makeApp } from './utils'
 
 describe('Middleware.wrapNative', () => {
-  describe('cors middleware', () => {
+  describe('cors', () => {
     const ORIGIN = 'http://example.com'
     const OPTIONS_STATUS = 234
 
@@ -38,6 +39,34 @@ describe('Middleware.wrapNative', () => {
     })
     it('preflight request', async () => {
       await request(app).options('/test').expect(OPTIONS_STATUS)
+    })
+  })
+
+  describe('helmet', () => {
+    const wrappedHelmet = Middleware.wrapNative(helmet())
+
+    const testRoute = route
+      .post('/test')
+      .use(wrappedHelmet)
+      .handler(async () => {
+        return Response.ok('success')
+      })
+
+    const app = makeApp().use(router(testRoute).handler())
+
+    it('works', async () => {
+      await request(app)
+        .post('/test')
+        .expect(200, 'success')
+        .expect('Referrer-Policy', 'no-referrer')
+        .expect((res) => {
+          const poweredBy = res.get('X-Powered-By')
+          if (poweredBy) {
+            throw new Error(
+              `X-Powered-By header should not be set but was ${poweredBy}`
+            )
+          }
+        })
     })
   })
 })
