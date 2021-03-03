3e9efe2d2f410e993d376398556d12803173b133
Petri Lehtinen <petri@digip.org>
2021-03-03 20:43:25 +0200

    Rehaul Middleware.wrapNative
    It's enough to replace res.end, so don't replace res.send and
    res.sendFile anymore.
    
    Support wrapping middlewares that themselves replace res.end. This is
    achieved by tracking whether this middleware has already
    "resolved" (i.e. called next()). If it has, the overridden res.end must
    call the original res.end instead of doing the typera specific
    middleware return handling.

--- packages/typera-express/src/middleware.ts
+++ packages/typera-express/src/middleware.ts
@@ -28,68 +28,37 @@ export const wrapNative = <Result = unknown>(
 ): Middleware<Result, never> => (request) =>
   new Promise((resolve, reject) => {
     const { req, res } = request
-    const makeResult = result ?? (() => ({} as Result))
+    let resolved = false
 
     const next = (err?: unknown) => {
-      restoreRes()
-
+      resolved = true
       if (err) {
         // middleware called next(err)
         reject(err)
       } else {
         // middleware called next()
+        const makeResult = result ?? (() => ({} as Result))
         resolve(commonMiddleware.next(makeResult(request)))
       }
     }
 
-    const end: EndHandler = (status, method, args) => {
-      // middleware sent the response
-
-      restoreRes()
-
-      // Abuse streamingBody to call an arbitrary res method
-      resolve(
-        commonMiddleware.stop({
-          status,
-          body: commonResponse.streamingBody(() => {
-            const resAny = res as any
-            resAny[method](...args)
-          }),
-        } as never)
-      )
+    const originalEnd = res.end
+    res.end = (...args: any): void => {
+      if (!resolved) {
+        resolved = true
+        // Abuse streamingBody to call an arbitrary res method
+        resolve(
+          commonMiddleware.stop({
+            status: res.statusCode,
+            body: commonResponse.streamingBody(() => {
+              originalEnd.apply(res, args)
+            }),
+          } as never)
+        )
+      } else {
+        originalEnd.apply(res, args)
+      }
     }
 
-    const restoreRes = setupResponseMock(res, end)
     middleware(req, res, next)
   })
-
-type EndHandler = (
-  status: number,
-  method: 'end' | 'send' | 'sendFile',
-  args: any[]
-) => void
-
-const setupResponseMock = (
-  res: express.Response,
-  onEnd: EndHandler
-): (() => void) => {
-  // These methods are the ones that express uses internally to send a
-  // response. Others are available, but they all just call these three.
-  const end = (...args: any[]): void => {
-    onEnd(res.statusCode, 'end', args)
-  }
-  const send = (body?: any): express.Response => {
-    onEnd(res.statusCode, 'send', [body])
-    return res
-  }
-  const sendFile = (...args: any[]): void => {
-    onEnd(res.statusCode, 'sendFile', args)
-  }
-
-  const originals = { end: res.end, send: res.send, sendFile: res.sendFile }
-  Object.assign(res, { end, send, sendFile })
-
-  return () => {
-    Object.assign(res, originals)
-  }
-}
