1fc8764a845c16d0e9c0d04f80d8ff2d44fa9a73
Joonas Rouhiainen <joonas.rouhiainen@iki.fi>
2022-02-22 20:50:02 +0200

    fix: Improve typings for parser#headers & headersP
    Any types prevented type inference of request.headers. Typing test was
    also using io-ts incorrectly, which was caught by stricter types.

--- typing-tests/tests/error-exact-header-codec.ts
+++ typing-tests/tests/error-exact-header-codec.ts
@@ -4,10 +4,10 @@ import { Parser, Response, Route, route } from 'typera-koa'
 export const handler: Route<Response.Ok<string> | Response.BadRequest<string>> =
   route
     .get('/')
-    .use(Parser.headers(t.exact({ foo: t.string })))
+    .use(Parser.headers(t.strict({ foo: t.string })))
     .handler((_req) => {
       return Response.ok('😎')
     })
 
 // Expected error:
-// Argument of type 'ExactC<HasProps>' is not assignable to parameter of type 'never'.
+// Argument of type 'ExactC<TypeC<{ foo: StringC; }>>' is not assignable to parameter of type 'TypeC<Props>'.
